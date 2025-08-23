af07609867e30409f85a74fd12dec791bd88c18e
cmmasaba <mmasabacollins9@gmail.com>
2025-08-23 23:34:44 +0300

    added linear pipelines and parallelism

--- gas-station/main.go
+++ gas-station/main.go
@@ -11,6 +11,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"math/rand"
 	"strconv"
@@ -112,10 +113,18 @@ func resetMachines() {
 	fuelPump.close()
 	airPump.close()
 	cleaner.close()
+
 	fuelPump = newMachine("fuelPump")
 	airPump = newMachine("airPump")
 	cleaner = newMachine("cleaner")
 }
+
+type (
+	fueled  int
+	cleaned int
+	pumped  int
+	served  int
+)
 
 func runPhase(d, jitter time.Duration) time.Duration {
 	if jitter > 0 {
@@ -123,15 +132,255 @@ func runPhase(d, jitter time.Duration) time.Duration {
 		d += time.Duration(rand.Int63n((jitter).Nanoseconds()))
 		d -= jitter / 2
 	}
+
 	start := time.Now()
+
 	useCPU(d)
+
 	return time.Since(start)
 }
 
-func pumpFuel(fuelPump *machine) {}
+func pumpFuel(fuelPump *machine) fueled {
+	fuelPump.add(runPhase(*fuelTime, *jitter))
+	return fueled(0)
+}
 
-func pumpAir(airPump *machine) {}
+func pumpAir(airPump *machine) pumped {
+	airPump.add(runPhase(*pumpTime, *jitter))
+	return pumped(0)
+}
 
-func cleanCar (cleaner *machine) {}
+func cleanCar(cleaner *machine) cleaned {
+	cleaner.add(runPhase(*cleanTime, *jitter))
+	return cleaned(0)
+}
 
-func promptPayment() {}
+func promptPayment(cleanedCar cleaned) served {
+	runPhase(*payTime, 0)
+	return served(int(cleanedCar))
+}
+
+// Station here refers to the whole set of machines required to perform these
+// tasks
+// Ideal case with no contention. Fails the race detector
+func idealStation() {
+	_ = pumpFuel(fuelPump)
+	_ = pumpAir(airPump)
+	cleanedCar := cleanCar(cleaner)
+	promptPayment(cleanedCar)
+}
+
+// Lock the whole station for each operation. Maximum contention
+var station sync.Mutex
+
+func lockingStation() {
+	station.Lock()
+	defer station.Unlock()
+
+	_ = pumpFuel(fuelPump)
+	_ = pumpAir(airPump)
+	cleanedCar := cleanCar(cleaner)
+	promptPayment(cleanedCar)
+}
+
+// Fine grain locking, only lock one machine. Reduces contention.
+func fineLockingStation() {
+	_ = lockingFuel()
+	_ = lockingAirPump()
+	cleanedCar := lockingClean()
+	promptPayment(cleanedCar)
+}
+
+func lockingFuel() fueled {
+	fuelPump.Lock()
+	defer fuelPump.Unlock()
+
+	return pumpFuel(fuelPump)
+}
+
+func lockingClean() cleaned {
+	cleaner.Lock()
+	defer cleaner.Unlock()
+
+	return cleanCar(cleaner)
+}
+
+func lockingAirPump() pumped {
+	airPump.Lock()
+	defer airPump.Unlock()
+
+	return pumpAir(airPump)
+}
+
+// Parallelize operations when there's available CPU
+// PumpAir, clean car when fueling it.
+func parallelAirPumping() served {
+	c := make(chan pumped, 1)
+
+	go func() {
+		c <- lockingAirPump()
+	}()
+
+	_ = lockingFuel()
+	cleanedCar := lockingClean()
+
+	<-c
+
+	return promptPayment(cleanedCar)
+}
+
+// Skip the air pumpimg stage to avoid CPU burning for that stage
+func skipAirPumping() served {
+	lockingFuel()
+
+	cleanedCar := lockingClean()
+
+	return promptPayment(cleanedCar)
+}
+
+// Simulate scenario for pumping fuel only, skipping air pumping and cleaning
+func fuelPumpOnly() served {
+	lockingFuel()
+
+	cleanedCar := cleaned(0)
+
+	return promptPayment(cleanedCar)
+}
+
+// Multiple machines to reduce contention
+var fuelPumps, airPumps, cleaners chan *machine
+
+// newMachines returns a channel containing n machines in its buffer.
+func newMachines(name string, n int) chan *machine {
+	c := make(chan *machine, n)
+
+	for i := range n {
+		c <- newMachine(fmt.Sprintf("%s%d", name, i))
+	}
+
+	return c
+}
+
+func closeMachines(c chan *machine) {
+	close(c)
+
+	for m := range c {
+		m.close()
+	}
+}
+
+func multiFuel() fueled {
+	m := <-fuelPumps
+
+	fuel := pumpFuel(m)
+	fuelPumps <- m
+
+	return fuel
+}
+
+func multiAirPump() pumped {
+	m := <-airPumps
+
+	pump := pumpAir(m)
+	airPumps <- m
+
+	return pump
+}
+
+func multiClean() cleaned {
+	m := <-cleaners
+
+	clean := cleanCar(m)
+	cleaners <- m
+
+	return clean
+}
+
+func multiStation() served {
+	multiFuel()
+	multiAirPump()
+
+	cleanedCar := multiClean()
+
+	return promptPayment(cleanedCar)
+}
+
+// Linear pipeline
+type order struct {
+	fuel  fueled
+	pump  pumped
+	clean chan cleaned
+}
+
+type linearPipeline struct {
+	fuelMachine     *machine
+	airPumpMachine  *machine
+	cleaningMachine *machine
+
+	orders          chan order
+	fueledOrders    chan order
+	airPumpedOrders chan order
+	done            chan int
+}
+
+func newLinearPipeline(buffer int) *linearPipeline {
+	p := &linearPipeline{
+		fuelMachine:     newMachine("fuelPump"),
+		airPumpMachine:  newMachine("airPump"),
+		cleaningMachine: newMachine("cleaner"),
+		orders:          make(chan order, buffer),
+		fueledOrders:    make(chan order, buffer),
+		airPumpedOrders: make(chan order, buffer),
+		done:            make(chan int),
+	}
+
+	go p.pumpFuel()
+	go p.pumpAir()
+	go p.clean()
+
+	return p
+}
+
+func (p *linearPipeline) station() served {
+	o := order{clean: make(chan cleaned, 1)}
+	p.orders <- o
+
+	clean := <-o.clean
+
+	return promptPayment(clean)
+}
+
+func (p *linearPipeline) pumpFuel() {
+	for o := range p.orders {
+		o.fuel = pumpFuel(p.fuelMachine)
+		p.fueledOrders <- o
+	}
+
+	close(p.fueledOrders)
+}
+
+func (p *linearPipeline) pumpAir() {
+	for o := range p.fueledOrders {
+		o.pump = pumpAir(p.airPumpMachine)
+		p.airPumpedOrders <- o
+	}
+
+	close(p.airPumpedOrders)
+}
+
+func (p *linearPipeline) clean() {
+	for o := range p.airPumpedOrders {
+		o.clean <- cleanCar(p.cleaningMachine)
+	}
+
+	close(p.done)
+}
+
+func (p *linearPipeline) close() {
+	close(p.orders)
+	<-p.done
+
+	p.fuelMachine.close()
+	p.airPumpMachine.close()
+	p.cleaningMachine.close()
+}
